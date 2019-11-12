2f281c4ca50a81cb39e39e27f4f5ab6564e5e0ae
M Ridwan Taufik Hidayat <mr.taufikhidayat.1995@gmail.com>
2019-11-12 16:10:55 +0700

    remove unused class

--- auth-server/src/main/java/com/daksa/oauth/service/AccessTokenService.java
+++ auth-server/src/main/java/com/daksa/oauth/service/AccessTokenService.java
@@ -3,11 +3,10 @@ package com.daksa.oauth.service;
 import com.daksa.oauth.domain.OAuthAccessToken;
 import com.daksa.oauth.exception.AuthorizationException;
 import com.daksa.oauth.exception.InvalidAuthCodeException;
-import com.daksa.oauth.exception.InvalidClientException;
 
 public interface AccessTokenService {
 	String createAuthCode(String code);
 	OAuthAccessToken requestTokenAuthCode(String clientId, String code, String codeVerifier) throws InvalidAuthCodeException;
-	OAuthAccessToken requestTokenClientCredential(String clientId, String clientSecret) throws InvalidClientException, AuthorizationException;
+	OAuthAccessToken requestTokenClientCredential(String clientId, String clientSecret) throws AuthorizationException;
 	OAuthAccessToken requestTokenRefreshToken(String clientId, String clientSecret, String refreshToken);
 }
